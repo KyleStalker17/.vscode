158038d91b32b6c6c0087acb901c53a8b3a15843
Donutbomb1 <skylewalker17@gmail.com>
2022-05-21 22:19:05 -0600

    master

---
+++ extensions/ms-python.vscode-pylance-2022.5.2/dist/typeshed-fallback/stubs/docopt/docopt.pyi
@@ -0,0 +1,11 @@
+from collections.abc import Iterable
+from typing import Any
+from typing_extensions import TypeAlias
+
+__version__: str
+
+_Argv: TypeAlias = Iterable[str] | str
+
+def docopt(
+    doc: str, argv: _Argv | None = ..., help: bool = ..., version: Any | None = ..., options_first: bool = ...
+) -> dict[str, Any]: ...  # Really should be dict[str, str | bool]
